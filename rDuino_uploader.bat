6a15eb42ad12e171c48da1694f4a77e9c671a02a
SebCanet <sebastien.canet@ac-nantes.fr>
2016-10-09 11:26:09 +0200

    Initial release

---
+++ rDuino_uploader.bat
@@ -0,0 +1,7 @@
+@echo off
+break ON
+rem fichiers BAT et fork créés par Sébastien CANET
+SET currentpath=%~dp1
+cd %currentpath%scripts
+cls
+python.exe .\rDuino\rDuino_uploader_server.py -D COM4 -U arduino_debug.exe -C arduino.exe -T F:\Logiciels\Arduino_graphique\Blockly-at-rduino_AIO\ArduinoTechnoEduc\
